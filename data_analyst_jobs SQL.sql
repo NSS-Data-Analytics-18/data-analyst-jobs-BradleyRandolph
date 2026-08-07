0e63f5c1e4bb52d58f9bbb99da288ca954f62d81
bradley randolph <charles.b.randolph@gmail.com>
2026-08-06 19:37:26 -0500

    Thursday project push

--- data_analyst_jobs SQL.sql
+++ data_analyst_jobs SQL.sql
@@ -47,8 +47,87 @@ GROUP BY location;
 
 
 
+SELECT DISTINCT title
+FROM data_analyst_jobs;
 
 
+
+SELECT COUNT(DISTINCT title) AS unique_job_titles
+FROM data_analyst_jobs;
+
+
+
+SELECT COUNT(DISTINCT title) AS CA_unique_job_titles
+FROM data_analyst_jobs
+WHERE location ='CA';
+
+
+
+SELECT company, AVG(star_rating), SUM(review_count)
+FROM data_analyst_jobs
+GROUP BY company
+HAVING SUM(review_count)>5000;
+
+
+SELECT company, ROUND(AVG(star_rating),2) AS avg_star_rating, 
+SUM(review_count) AS review_count
+FROM data_analyst_jobs
+GROUP BY company
+HAVING SUM(review_count)>5000;
+
+
+
+SELECT company, ROUND(AVG(star_rating),2) AS avg_star_rating, 
+SUM(review_count) AS review_count
+FROM data_analyst_jobs
+GROUP BY company
+HAVING SUM(review_count)>5000
+ORDER BY review_count DESC;
+
+
+SELECT title
+FROM data_analyst_jobs
+WHERE title ILIKE '%analyst%';
+
+
+
+SELECT COUNT(title) AS analyst_jobs
+FROM data_analyst_jobs
+WHERE title ILIKE '%analyst%';
+
+
+
+
+SELECT title
+FROM data_analyst_jobs
+WHERE title NOT ILIKE '%analyst%'
+   AND title NOT ILIKE '%analytics%';
+
+
+
+
+SELECT skill
+FROM data_analyst_jobs
+WHERE skill ILIKE '%SQL%';
+
+
+SELECT domain, COUNT(*) AS job_count
+FROM data_analyst_jobs
+WHERE skill ILIKE '%SQL%'
+   AND days_since_posting >21
+   AND domain IS NOT NULL
+   GROUP BY domain;
+
+
+
+SELECT domain, COUNT(*) AS job_count
+FROM data_analyst_jobs
+WHERE skill ILIKE '%SQL%'
+   AND days_since_posting >21
+   AND domain IS NOT NULL
+   GROUP BY domain
+   ORDER BY job_count DESC
+   LIMIT 3;
 
 
 
